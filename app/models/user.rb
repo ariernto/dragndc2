8a6e0d028cbee0f3ad7293de854cfb5f6e1ce48f
rh4y <rh7214you@gmail.com>
2022-06-29 02:18:52 -0400

    Working on Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-    
+    has_secure_password
+
+    validates :password, length: { minimum: 6 }
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify' to: 'authentication#verify'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
