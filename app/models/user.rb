1026598bd0ebb3b30dfe99c5772558d808c09cd1
rh4y <rh7214you@gmail.com>
2022-06-29 03:02:05 -0400

    Working on Password Digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :characters, dependent: :destroy
 
-    validates :password, length: { minimum: 6 }
+    # validates :password, length: { minimum: 6 }
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
