1e4b6225ed8a5893d5f158c0b77cf2f49f40a21c
rh4y <rh7214you@gmail.com>
2022-06-29 02:38:41 -0400

    Added Character Stats

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :characters, dependent: :destroy
 
     validates :password, length: { minimum: 6 }
     validates :username, presence: true, uniqueness: true
