5ff4ffe214b9ed91efa7ba190a8740de3ca1944d
rh4y <rh7214you@gmail.com>
2022-08-30 19:02:31 -0400

    Added Logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Home from './screens/Home/Home';
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/Creator" element={<h1>Character Creation</h1>} />
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+
+export default function Navbar() {
+  return (
+    <div>
+        <Link to={'/'} ><img src="https://github.com/Jacobdye99/DnDCC2/blob/main/Assets/dungeons-and-dragons-723167.png?raw=true" alt="DnD Logo" id="Logo" /></Link>
+    </div>
+  )
+}
