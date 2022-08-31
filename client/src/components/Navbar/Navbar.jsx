40509afb81430cd621bc46c0430dbd4535bc8b66
rh4y <rh7214you@gmail.com>
2022-08-30 23:35:45 -0400

    Added Logo

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css"
 
 
 export default function Navbar() {
   return (
     <div>
-        <Link to={'/'} ><img src="https://github.com/Jacobdye99/DnDCC2/blob/main/Assets/dungeons-and-dragons-723167.png?raw=true" alt="DnD Logo" id="Logo" /></Link>
+        <Link to={'/'} id="Logo" ><img src="https://github.com/Jacobdye99/DnDCC2/blob/main/Assets/dungeons-and-dragons-723167.png?raw=true" alt="DnD Logo" id="Logo" /></Link>
     </div>
   )
 }
