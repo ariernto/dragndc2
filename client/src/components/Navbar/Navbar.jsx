dfdc5df1667d6df71d2ae58d98c55f9b1639386d
rh4y <rh7214you@gmail.com>
2022-09-08 21:49:11 -0400

    Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,8 +4,9 @@ import "./Navbar.css"
 
 export default function Navbar() {
   return (
-    <div>
-        <Link to={'/'} id="Logo" ><img src="https://github.com/Jacobdye99/DnDCC2/blob/main/Assets/dungeons-and-dragons-723167.png?raw=true" alt="DnD Logo" id="Logo" /></Link>
+    <div className="Top">
+        <Link to={'/'} id="Logo" ><img src="https://github.com/Jacobdye99/DnDCC2/blob/main/Assets/dungeons-and-dragons-723167.png?raw=true" alt="DnD Logo" id="Logo" /><h3 className="CC">Character Creator</h3></Link>
+        
     </div>
   )
 }
